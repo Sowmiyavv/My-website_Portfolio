e0edfad944e0030d5f9a09fa02ec6d8bf31a8b50
Sowmiyavv <sowmiyavv2017@gmail.com>
2021-04-04 23:47:01 +0530

    first_commit

---
+++ portfolio_js.js
@@ -0,0 +1,24 @@
+function callHome(){
+			var elmnt = document.getElementById("profile");
+			elmnt.scrollIntoView();
+		}
+
+		function callAbout(){
+			var elmnt = document.getElementById("about");
+			elmnt.scrollIntoView();
+		}
+
+		function callResume(){
+			var elmnt = document.getElementById("education");
+			elmnt.scrollIntoView();
+		}
+
+		function callProject(){
+			var elmnt = document.getElementById("project");
+			elmnt.scrollIntoView();
+		}
+
+		function callContact(){
+			var elmnt = document.getElementById("contact");
+			elmnt.scrollIntoView();
+		}
